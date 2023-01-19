eb93eb3f1c27dca7ffbb55c87a6d6e2bfb162628
kychmit <katrustim@gmail.com>
2023-01-19 17:47:06 +0200

    commit

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,20 +1,22 @@
 import {User} from "../User/User";
 import {useEffect, useState} from "react";
+// import axios from "axios";
+import {axiosService} from "../../services/axiosService";
 
 const Users = () => {
 
     const [users, setUsers] = useState([]);
+    const [count, setCount ] = useState(0);
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(value => value.json())
-            .then(value => setUsers(value))
-                          }, [])
-
-
+       axiosService.get('/users')
+            .then(value => value.data)
+            .then(value => setUsers([...value]))
+                          }, [count])
 
     return (
         <div>
+            <button onClick={() => setCount(prevState => prevState+1)}></button>
             {users.map(user => <User key={user.id} user={user}/>)}
         </div>
     );
